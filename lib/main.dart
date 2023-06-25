63862c2be27dbe03e275e78bff8ac015d9507f58
athiruj.k <athirujkaewseesuk@gmail.com>
2023-06-25 23:15:53 +0700

    Updete UI

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool connnect = false;
+  bool ledState = false;
   TextEditingController uriController = TextEditingController();
 
   void connectSocket(String strUri) async {
-    final channel = WebSocketChannel.connect(Uri.parse(strUri));
-    channel.stream.listen((event) {
-      channel.sink.add("received");
-      channel.sink.close(status.goingAway);
-    });
+    print("ws://${strUri}");
+    // final channel = WebSocketChannel.connect(Uri.parse(strUri));
+    // channel.stream.listen((event) {
+    //   channel.sink.add("received");
+    //   channel.sink.close(status.goingAway);
+    // });
   }
 
   @override
@@ -49,113 +52,122 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  bool connnect = false;
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: GoogleFonts.kanit(fontSize: 18),
-      child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints viewportConstraints) {
-        return Scaffold(
-          body: SingleChildScrollView(
-            child: Center(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: viewportConstraints.maxHeight,
-                  maxWidth: 300,
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      // ! Setting part
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          SizedBox(
-                            height: 56,
-                            width: 300,
-                            child: TextField(
-                              controller: uriController,
-                              textAlign: TextAlign.center,
-                              decoration: InputDecoration(
-                                hintText: "Enter ip Address",
-                                hintStyle: GoogleFonts.kanit(fontSize: 16),
-                              ),
-                              onSubmitted: (value) {
-                                connectSocket(value);
-                              },
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+      return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+                maxWidth: 300,
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    // ! Setting part
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 56,
+                          width: 300,
+                          child: TextField(
+                            controller: uriController,
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.inter(fontSize: 18),
+                            decoration: InputDecoration(
+                              
+
+                              hintText: "Enter ip Address",
+                              hintStyle: GoogleFonts.inter(fontSize: 18),
                             ),
                           ),
-                          TextButton(
-                            child: Container(
-                              width: 300,
-                              decoration: const BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Text(
-                                  "Connect",
-                                  style: GoogleFonts.kanit(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          child: FilledButton(
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              fixedSize: MaterialStateProperty.resolveWith(
+                                  (states) => const Size(300, 48)),
+                              backgroundColor:
+                                  MaterialStateColor.resolveWith((states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Colors.blueGrey;
+                                }
+                                return Colors.blue;
+                              }),
                             ),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      // ! Action part
-                      Column(
-                        children: [
-                          Text(
+                            child: Text(
+                              "Connect",
+                              style: GoogleFonts.inter(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            onPressed: () {
+                              connectSocket(uriController.text);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    // ! Action part
+                    Column(
+                      children: [
+                        Text(
                             connnect
                                 ? "Status: Connected"
                                 : "Status: Disconnect",
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          TextButton(
-                            child: Container(
-                              width: 300,
-                              decoration: const BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Text(
-                                  "On",
-                                  style: GoogleFonts.kanit(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
+                            style: GoogleFonts.inter(fontSize: 18)),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          child: TextButton(
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              fixedSize: MaterialStateProperty.resolveWith(
+                                  (states) => const Size(300, 48)),
+                              backgroundColor:
+                                  MaterialStateColor.resolveWith((states) {
+                                if (states.contains(MaterialState.pressed)) {
+                                  return Colors.blueGrey;
+                                }
+                                return ledState ? Colors.blue : Colors.blueGrey;
+                              }),
                             ),
-                            onPressed: () {},
-                          )
-                        ],
-                      )
-                    ]),
-              ),
+                            child: Text(
+                              ledState ? "On" : "Off",
+                              style: GoogleFonts.inter(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                ledState = !ledState;
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    )
+                  ]),
             ),
           ),
-        );
-      }),
-    );
+        ),
+      );
+    });
   }
 }
