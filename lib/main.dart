c919ee8277c3d58eaa138b9b21c1581bfed9f6c6
athiruj.k <athirujkaewseesuk@gmail.com>
2023-06-25 21:36:13 +0700

    Edit UI Part

--- lib/main.dart
+++ lib/main.dart
@@ -35,88 +35,105 @@ class _MyHomePageState extends State<MyHomePage> {
   bool connnect = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Column(
-                children: [
-                  SizedBox(
-                    height: 56,
-                    width: 300,
-                    child: TextField(
-                      controller: uriController,
-                      decoration: InputDecoration(
-                        hintText: "Enter ip Address",
-                        hintStyle: GoogleFonts.kanit(fontSize: 16),
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    child: Container(
-                      width: 300,
-                      decoration: const BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          "Connect",
-                          style: GoogleFonts.kanit(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    onPressed: () {},
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                child: Column(
-                  children: [
-                    Text(
-                      "Status:",
-                      style: GoogleFonts.kanit(fontSize: 18),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextButton(
-                      child: Container(
-                        width: 300,
-                        decoration: const BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text(
-                            "On",
-                            style: GoogleFonts.kanit(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
+    return DefaultTextStyle(
+      style: GoogleFonts.kanit(fontSize: 18),
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+        return Scaffold(
+          body: SingleChildScrollView(
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                  maxWidth: 300,
                 ),
-              )
-            ]),
-      ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      // ! Setting part
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          SizedBox(
+                            height: 56,
+                            width: 300,
+                            child: TextField(
+                              controller: uriController,
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                hintText: "Enter ip Address",
+                                hintStyle: GoogleFonts.kanit(fontSize: 16),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            child: Container(
+                              width: 300,
+                              decoration: const BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  "Connect",
+                                  style: GoogleFonts.kanit(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      // ! Action part
+                      Column(
+                        children: [
+                          const Text(
+                            "Status:",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextButton(
+                            child: Container(
+                              width: 300,
+                              decoration: const BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  "On",
+                                  style: GoogleFonts.kanit(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            onPressed: () {},
+                          )
+                        ],
+                      )
+                    ]),
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
